33ac94d22e5903fe920c39d4746b3b8bee80e150
Anurag Yadav <anu17.yadav1@gmail.com>
2023-05-18 21:43:55 +0800

    Basic html handles added

---
+++ script.js
@@ -0,0 +1,10 @@
+function submitAnswer() {
+    // Add logic to handle answer submission
+  }
+  
+  function loadNextQuestion() {
+    // Add logic to load the next question or challenge
+  }
+  
+  // Add other functions and event listeners as needed
+  
